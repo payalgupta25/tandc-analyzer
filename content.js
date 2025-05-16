3a645950c5914a8e0e64d282d0abd30adaf97549
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-05-16 21:26:19 +0530

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-console.log("Content script loaded.");
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getPageText") {
-      const visibleText = document.body.innerText;
-      sendResponse({ text: visibleText });
-    }
-  });
-  
