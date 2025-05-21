aa69a8ff8b5686fe7f4e05ba90ba63a981275a65
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2025-05-21 15:06:08 +0530

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,9 @@
+// content.js
 console.log("📄 content.js loaded");
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "getPageText") {
+    // grab every bit of visible text on the page
     const visibleText = document.body.innerText;
     sendResponse({ text: visibleText });
   }
